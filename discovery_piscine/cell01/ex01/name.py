b12dba1c048ec495fcb2e7076757c31302faca57
IT21Neo <66070320@kmitl.ac.th>
2026-09-21 14:55:38 +0700

    ex01: name.py ex02: age.py

---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Akkarachai"
+last_name = "Junchomchuen"
+whole_name = first_name + " " + last_name
+print(whole_name)
---
+++ discovery_piscine/cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age = 22
+added_years = 42
+my_age = age + added_years
+print(my_age)
